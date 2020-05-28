c47cbe37b0a7aeaa4637f7977a6d82f88a4ddc07
JacobVerona <jveronajacob@gmail.com>
2020-05-28 12:23:08 +0300

    Init setup

---
+++ deanoffice_moblie/app/src/main/java/chdtu/fitis/deanoffice_moblie/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package chdtu.fitis.deanoffice_moblie.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+
+    }
+    val text: LiveData<String> = _text
+}
