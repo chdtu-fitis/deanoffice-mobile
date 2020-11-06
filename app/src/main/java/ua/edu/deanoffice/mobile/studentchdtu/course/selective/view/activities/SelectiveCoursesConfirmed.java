28b7eeb16be83f79a09f08ddf5de3d80e2198718
AndrewZolotoverkhyi <a-zolotoverhii@ukr.net>
2020-11-06 11:15:34 +0200

    #32 creating interface: add: activity class, activity layout

---
+++ app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/course/selective/view/activities/SelectiveCoursesConfirmed.java
@@ -0,0 +1,17 @@
+package ua.edu.deanoffice.mobile.studentchdtu.course.selective.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ua.edu.deanoffice.mobile.studentchdtu.R;
+
+public class SelectiveCoursesConfirmed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selective_courses_confirmed);
+
+    }
+}
