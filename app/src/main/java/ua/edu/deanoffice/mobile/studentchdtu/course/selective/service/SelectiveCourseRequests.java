e7d9c898a6a042ef69293063d98d158463ab5d8f
AndrewZolotoverkhyi <a-zolotoverhii@ukr.net>
2020-11-26 11:06:10 +0200

    #41 fix: crash

--- app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/course/selective/service/SelectiveCourseRequests.java
+++ app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/course/selective/service/SelectiveCourseRequests.java
@@ -9,11 +9,17 @@ import retrofit2.http.Query;
 import ua.edu.deanoffice.mobile.studentchdtu.course.selective.model.ConfirmedSelectiveCourses;
 import ua.edu.deanoffice.mobile.studentchdtu.course.selective.model.SelectiveCourses;
 import ua.edu.deanoffice.mobile.studentchdtu.course.selective.model.SelectiveCoursesStudentDegree;
+import ua.edu.deanoffice.mobile.studentchdtu.course.selective.model.StudentDegreeSelectiveCoursesIds;
 
 public interface SelectiveCourseRequests {
 
     @GET("selective-courses")
     Call<SelectiveCourses> requestSelectiveCourses(@Header("Authorization") String token, @Query("studentDegreeId") int id);
+
+    @GET("selective-courses/student-degree")
+    Call<SelectiveCoursesStudentDegree> studentDegree(@Header("Authorization") String token, @Query("studentDegreeId") int id);
+
     @POST("selective-courses/registration")
-    Call<SelectiveCoursesStudentDegree> confirmedSelectiveCourses(@Header("Authorization") String token, @Body ConfirmedSelectiveCourses selectiveCourses);
+    Call<StudentDegreeSelectiveCoursesIds> confirmedSelectiveCourses(@Header("Authorization") String token, @Body ConfirmedSelectiveCourses selectiveCourses);
+
 }
