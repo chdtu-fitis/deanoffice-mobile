bf64e0f1eab0da23b357a6b04cc5dd64c007a5e7
iProsto <lenovo.arxi@gmail.com>
2021-04-21 22:55:26 +0300

    #51 new option notification selective courses registration: add: options for enabled or disabled notification for reminder about registration selective courses; fix: drawer-menu do not clear after press Back to MainMenuActivity

--- app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/user/profile/controller/OptionsController.java
+++ app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/user/profile/controller/OptionsController.java
@@ -1,5 +1,37 @@
 package ua.edu.deanoffice.mobile.studentchdtu.user.profile.controller;
 
-public class OptionsController {
+import android.content.Context;
+import android.content.SharedPreferences;
 
+import ua.edu.deanoffice.mobile.studentchdtu.user.profile.model.MainOptions;
+
+public class OptionsController {
+    private final Context context;
+    private final SharedPreferences sharedPreferences;
+
+    public OptionsController(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(MainOptions.APP_MAIN_OPTIONS, Context.MODE_PRIVATE);
+    }
+
+    //Save and Load need update always when added new option
+    public void save(MainOptions mainOptions){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putBoolean(MainOptions.ENABLE_NOTIF_ON_REG_SELECTIVE_COURSES, mainOptions.isEnableNotifOnRegSelectiveCourses());
+
+        editor.apply();
+    }
+
+    public MainOptions load(){
+        MainOptions mainOptions = new MainOptions();
+
+        String optionId = MainOptions.ENABLE_NOTIF_ON_REG_SELECTIVE_COURSES;
+        if(sharedPreferences.contains(optionId)) {
+            boolean value = sharedPreferences.getBoolean(optionId, mainOptions.isEnableNotifOnRegSelectiveCourses());
+            mainOptions.setEnableNotifOnRegSelectiveCourses(value);
+        }
+
+        return mainOptions;
+    }
 }
