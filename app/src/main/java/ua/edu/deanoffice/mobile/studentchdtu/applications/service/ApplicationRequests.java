9b6c577d2090aa1859422dc73765e0b7cc66a8ed
gzaspa <mshvenra5>
2021-07-12 17:03:00 +0300

    applications: fix: get back application types GET request method

--- app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/applications/service/ApplicationRequests.java
+++ app/src/main/java/ua/edu/deanoffice/mobile/studentchdtu/applications/service/ApplicationRequests.java
@@ -7,10 +7,15 @@ import retrofit2.http.Headers;
 import retrofit2.http.Query;
 import ua.edu.deanoffice.mobile.studentchdtu.BuildConfig;
 import ua.edu.deanoffice.mobile.studentchdtu.applications.model.Application;
+import ua.edu.deanoffice.mobile.studentchdtu.applications.model.ApplicationTypePOJO;
 
 public interface ApplicationRequests {
 
     @GET("applications")
     @Headers("X-App-Version: " + BuildConfig.VERSION_NAME)
     Call<Application> requestStudentInfo(@Query("json") String json, @Query("applicationType") int id, @Header("Authorization") String token);
+
+    @GET("application-types")
+    @Headers("X-App-Version: " + BuildConfig.VERSION_NAME)
+    Call<ApplicationTypePOJO> requestApplicationTypes();
 }
